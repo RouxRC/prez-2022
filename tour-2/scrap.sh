cc122822402ea728e61bdcb2e173627c31fa8d7e
RouxRC <b.ooghe@gmail.com>
2022-04-24 21:25:19 +0200

    first results

--- tour-2/scrap.sh
+++ tour-2/scrap.sh
@@ -6,7 +6,7 @@ echo "candidat;voix;%inscrits;%exprimés" > resultats.csv
 curl -s https://www.resultats-elections.interieur.gouv.fr/presidentielle-2022/FE.html |
  iconv -f iso-8859-15       |
  grep '<td style'           |
- head -n 48                 |
+ head -n 8                  |
  sed 's/\s*<[^>]*>\s*//g'   |
  tr "\n" ";"                |
  sed 's/;$//'               |
@@ -23,7 +23,7 @@ function extract {
   sed -i '$s/$/,'"$value"'/' $out
 }
 
-if git diff resultats.csv | grep . > /dev/null && grep "ARTHAUD" resultats.csv > /dev/null; then
+if git diff resultats.csv | grep . > /dev/null && grep "MACRON" resultats.csv > /dev/null; then
 
   datetime=$(date --iso-8601=minutes)
   echo "$datetime" >> historique-voix.csv
